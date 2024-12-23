1721914bce35f8fe3fefc68ac61adabe15176fd5
zhengpeng <847850277@qq.com>
2024-12-23 16:42:42 +0800

    [ISSUES#9] to str.

--- deploy-sepolia/src/util.rs
+++ deploy-sepolia/src/util.rs
@@ -1,7 +1,22 @@
 use starknet::core::types::Felt;
+use starknet::core::utils::{cairo_short_string_to_felt, parse_cairo_short_string};
 
-pub fn get_str_from_felt(felt: Felt) -> String {
+pub async fn get_str_from_felt(felt: Felt) -> String {
     // to Str
     //0x7a1c71029f2d0b38e3ac89b09931d08b6e48417e079c289ff19a8698d0cba33
-    felt.to_hex_string()
+    let result = parse_cairo_short_string(&felt);
+    match result {
+        Ok(s) => {
+            return s;
+        }
+        Err(e) => {
+            return "".to_string();
+        }
+    }
+}
+
+pub async fn get_felt_from_str(str: &str) -> Felt {
+    // to felt
+    cairo_short_string_to_felt(str).unwrap()
+
 }
