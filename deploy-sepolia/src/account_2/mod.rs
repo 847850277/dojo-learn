fb12634ba424005fdf534d6c3e00133b079051b9
zhengpeng <847850277@qq.com>
2024-12-23 18:00:35 +0800

    [ISSUES#12] account_2_register.

---
+++ deploy-sepolia/src/account_2/mod.rs
@@ -0,0 +1,17 @@
+pub mod account_2_register;
+
+pub(crate) async fn test() {
+    //account_2::account_2_register::decare().await;
+    //account_2::account_2_register::deploy().await;
+    //account_2_register::register_namespace().await;
+    //account_2_register::register_model_decare_move().await;
+    //account_2_register::register_model_move().await;
+    //account_2_register::register_model_decare_position().await;
+    //account_2_register::register_model_position().await;
+    //account_2_register::register_model_decare_DirectionsAvailable().await;
+    //account_2_register::register_model_DirectionsAvailable().await;
+    //account_2_register::register_event_decare_move().await;
+    //account_2_register::register_event_move().await;
+    //account_2_register::register_contract_decare_actions().await;
+    account_2_register::register_contract_actions().await;
+}
