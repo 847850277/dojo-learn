25dd45b7c52a4d8111015d03657d75698f3340ee
zhengpeng <847850277@qq.com>
2024-12-24 21:02:49 +0800

    [ISSUES#18] spawn && move

--- deploy-sepolia/src/account_2/account_1_contract_call.rs
+++ deploy-sepolia/src/account_2/account_1_contract_call.rs
@@ -8,8 +8,47 @@ use starknet::providers::JsonRpcClient;
 use starknet::signers::{LocalWallet, SigningKey};
 use url::Url;
 
-pub(crate) async fn spawn_1_call() {
-    todo!()
+// 账户1调用合约的spawn函数
+pub(crate) async fn spawn_call() {
+    let provider = JsonRpcClient::new(HttpTransport::new(
+        Url::parse("https://starknet-sepolia.public.blastapi.io/rpc/v0_7").unwrap(),
+    ));
+
+    let signer = LocalWallet::from(SigningKey::from_secret_scalar(
+        Felt::from_hex("0x025e5b9982a2c8e04cb477d7c71aec25e2043e4d52cb61604208e1939acfb8bf").unwrap(),
+    ));
+    let address = Felt::from_hex("0x0156c66218B0836d8d49096529BBA0E750Eb36377E5a98F99A70ee997296D36a").unwrap();
+    let contract_address =
+        Felt::from_hex("0x023edf322be9eb54e407080d6eda97a2a5d3262a823bc9ae94fbac22a3464d9a").unwrap();
+
+    let mut account = SingleOwnerAccount::new(
+        provider,
+        signer,
+        address,
+        chain_id::SEPOLIA,
+        ExecutionEncoding::New,
+    );
+
+    // `SingleOwnerAccount` defaults to checking nonce and estimating fees against the latest
+    // block. Optionally change the target block to pending with the following line:
+    account.set_block_id(BlockId::Tag(BlockTag::Pending));
+
+    let result = account
+        .execute_v1(vec![Call {
+            to: contract_address,
+            selector: get_selector_from_name("spawn").unwrap(),
+            calldata: vec![
+                // address,
+                // Felt::from_dec_str("1000000000000000000000").unwrap(),
+                // Felt::ZERO,
+            ],
+        }])
+        .send()
+        .await
+        .unwrap();
+
+    println!("Transaction hash: {:#064x}", result.transaction_hash);
+
 }
 
 pub(crate) async fn move_call() {
@@ -18,7 +57,7 @@ pub(crate) async fn move_call() {
     ));
 
     let signer = LocalWallet::from(SigningKey::from_secret_scalar(
-        Felt::from_hex("0x028a46eddc7615d00e21d31dc959d2721c3cc5b267e381b7fd4c7931f3e61dfe").unwrap(),
+        Felt::from_hex("0x025e5b9982a2c8e04cb477d7c71aec25e2043e4d52cb61604208e1939acfb8bf").unwrap(),
     ));
     let address = Felt::from_hex("0x04097f4882C50bDdBaFe1A79337bDaBDf001456430aDede37F36E47E22d135De").unwrap();
     let contract_address =
@@ -41,7 +80,7 @@ pub(crate) async fn move_call() {
             to: contract_address,
             selector: get_selector_from_name("move").unwrap(),
             calldata: vec![
-                felt!("2")
+                felt!("1")
                 // Felt::from_dec_str("1000000000000000000000").unwrap(),
                 // Felt::ZERO,
             ],
--- deploy-sepolia/src/account_2/mod.rs
+++ deploy-sepolia/src/account_2/mod.rs
@@ -1,6 +1,7 @@
 pub mod account_2_register;
 pub mod account_2_permission;
 mod account_2_contract_call;
+mod account_1_contract_call;
 
 pub(crate) async fn test() {
     //account_2::account_2_register::decare().await;
@@ -16,5 +17,9 @@ pub(crate) async fn test() {
     //account_2_register::register_event_move().await;
     //account_2_register::register_contract_decare_actions().await;
     //account_2_register::register_contract_actions().await;
-    account_2_permission::permissions().await;
+    //account_2_permission::permissions().await;
+    //account_2_contract_call::spawn_1_call().await;
+    account_2_contract_call::move_call().await;
+    //account_1_contract_call::spawn_call().await;
+    //account_1_contract_call::move_call().await;
 }
